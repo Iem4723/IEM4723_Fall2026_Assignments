e561ecc44e6aa214ef05354532275274ab996516
Nikolas Rhodes <nikrhodes2@mac.eduroam-corewlcs-13.okstate.edu>
2026-09-15 14:09:10 -0500

    Complete Question 4 port forwarding

--- docker_commands.sh
+++ docker_commands.sh
@@ -18,3 +18,8 @@ cd assignment1/layering
 docker build -t logistic_regression_layered:latest .
 docker run --rm --volume "$(pwd)/data:/home/iem4723/data" logistic_regression_layered:latest
 
+# Question 4
+cd assignment1/port_listening
+docker build -t simple_file_server .
+docker run --rm --publish 3333:8000 simple_file_server
+
