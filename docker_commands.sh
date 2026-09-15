212cbd13184db6619430577b53322dac0e2e7f17
Nikolas Rhodes <nikrhodes2@mac.eduroam-corewlcs-13.okstate.edu>
2026-09-15 12:44:34 -0500

    Complete Question 2 volume mounting

---
+++ docker_commands.sh
@@ -0,0 +1,14 @@
+# Question 1
+cd assignment1/regression_container
+docker build -t linear-regression-app:latest .
+docker tag linear-regression-app:latest linear-regression-app:v0.1.1
+docker tag linear-regression-app:latest linear-regression-app:v0.1.2
+docker tag linear-regression-app:latest nikrhodes67/linear-regression-app:latest
+docker push nikrhodes67/linear-regression-app:latest
+cd ../..
+
+# Question 2
+cd assignment1/volume-mounting
+docker build -t volume-mounting-app .
+docker run --rm --volume "$(pwd)/data:/home/iem4723/data" volume-mounting-app:latest
+cd ../..
